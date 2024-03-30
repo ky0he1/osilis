14a4bd8379915d80cee583a35b03ceaf3e8780d3
ky0he1 <kyouta1218@gmail.com>
2024-03-31 01:30:57 +0900

    初めてのカーネル

--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# kernelのビルド
+cd $HOME/github/osilis/kernel
+clang++ -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone \
+    -fno-exceptions -fno-rtti -std=c++17 -c main.cpp
+ld.lld --entry KernelMain -z norelro -z separate-code \
+    --image-base 0x100000 --static -o kernel.elf main.o
+
 cd $HOME/edk2
 # 初回のみ
 # ln -s $HOME/github/osilis/OsilisLoaderPkg ./
 source edksetup.sh
 build
-$HOME/osbook/devenv/run_qemu.sh Build/OsilisLoaderX64/DEBUG_CLANG38/X64/Loader.efi
+$HOME/osbook/devenv/run_qemu.sh \
+    Build/OsilisLoaderX64/DEBUG_CLANG38/X64/Loader.efi \
+    $HOME/github/osilis/kernel/kernel.elf
