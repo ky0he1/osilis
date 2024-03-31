ea5934c20147c71183f579abae26d720a7d7dcd2
ky0he1 <kyouta1218@gmail.com>
2024-03-31 21:20:54 +0900

    make入門

--- run.sh
+++ run.sh
@@ -3,10 +3,7 @@
 # kernelのビルド
 source $HOME/osbook/devenv/buildenv.sh
 cd $HOME/github/osilis/kernel
-clang++ $CPPFLAGS -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone \
-    -fno-exceptions -fno-rtti -std=c++17 -c main.cpp
-ld.lld $LDFLAGS --entry KernelMain -z norelro -z separate-code \
-    --image-base 0x100000 --static -o kernel.elf main.o
+make
 
 cd $HOME/edk2
 # 初回のみ
