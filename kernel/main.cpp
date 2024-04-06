3ec2417cf3cddce4d96448e426531c79ce879e22
ky0he1 <kyouta1218@gmail.com>
2024-04-06 09:18:25 +0900

    分割コンパイル

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -7,82 +7,8 @@
 #include <cstddef>
 
 #include "frame_buffer_config.hpp"
-
-const uint8_t kFontA[16] = {
-    0b00000000, //
-    0b00011000, //    **
-    0b00011000, //    **
-    0b00011000, //    **
-    0b00011000, //    **
-    0b00100100, //   *  *
-    0b00100100, //   *  *
-    0b00100100, //   *  *
-    0b00100100, //   *  *
-    0b01111110, //  ******
-    0b01000010, //  *    *
-    0b01000010, //  *    *
-    0b01000010, //  *    *
-    0b11100111, // ***  ***
-    0b00000000, //
-    0b00000000, //
-};
-
-struct PixelColor {
-    uint8_t r, g, b;
-};
-
-class PixelWriter {
-  public:
-    PixelWriter(const FrameBufferConfig& config)
-        : config_{ config } {}
-    virtual ~PixelWriter() = default;
-    virtual void Write(int x, int y, const PixelColor& c) = 0;
-
-  protected:
-    uint8_t* PixelAt(int x, int y) {
-        return config_.frame_buffer + 4 * (config_.pixels_per_scan_line * y + x);
-    }
-
-  private:
-    const FrameBufferConfig& config_;
-};
-
-class RGBResv8BitPerColorPixelWrite : public PixelWriter {
-  public:
-    using PixelWriter::PixelWriter;
-
-    virtual void Write(int x, int y, const PixelColor& c) override {
-        auto p = PixelAt(x, y);
-        p[0] = c.r;
-        p[1] = c.g;
-        p[2] = c.b;
-    }
-};
-
-class BGRResv8BitPerColorPixelWrite : public PixelWriter {
-  public:
-    using PixelWriter::PixelWriter;
-
-    virtual void Write(int x, int y, const PixelColor& c) override {
-        auto p = PixelAt(x, y);
-        p[0] = c.b;
-        p[1] = c.g;
-        p[2] = c.r;
-    }
-};
-
-void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& color) {
-    if (c != 'A') {
-        return;
-    }
-    for (int dy = 0; dy < 16; ++dy) {
-        for (int dx = 0; dx < 8; ++dx) {
-            if ((kFontA[dy] << dx) & 0x80u) {
-                writer.Write(x + dx, y + dy, color);
-            }
-        }
-    }
-}
+#include "graphics.hpp"
+#include "font.hpp"
 
 void* operator new(size_t size, void* buf) {
     return buf;
@@ -90,18 +16,18 @@ void* operator new(size_t size, void* buf) {
 
 void operator delete(void* obj) noexcept {}
 
-char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWrite)];
+char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
 
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     switch (frame_buffer_config.pixel_format) {
         case kPixelRGBResv8BitPerColor:
             pixel_writer =
-                new (pixel_writer_buf) RGBResv8BitPerColorPixelWrite{ frame_buffer_config };
+                new (pixel_writer_buf) RGBResv8BitPerColorPixelWriter{ frame_buffer_config };
             break;
         case kPixelBGRResv8BitPerColor:
             pixel_writer =
-                new (pixel_writer_buf) BGRResv8BitPerColorPixelWrite{ frame_buffer_config };
+                new (pixel_writer_buf) BGRResv8BitPerColorPixelWriter{ frame_buffer_config };
             break;
         default:
             break;
